77f1d411c88873c3c0877b72e354de5ae7c3adda
Malibongwe <ap.mshumpela@gmail.com>
2025-11-20 10:48:10 +0200

    Deleted some classes

--- src/test/java/Tests/Base.java
+++ src/test/java/Tests/Base.java
@@ -1,41 +1,25 @@
 package Tests;
 
 import Pages.*;
+import Utils.BrowserFactory;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 
 public class Base {
 
-  WebDriver driver;
 
-    // All page objects - initialized in setUp
+    BrowserFactory browserFactory = new BrowserFactory();
+
+    final WebDriver driver = browserFactory.startBrowser("chrome", "https://www.ndosiautomation.co.za/");
 
 
-    @BeforeMethod
-    public void setUp() {
-        // Initialize browser
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://your-inventory-app.com");
+
 
         // Initialize ALL page objects using PageFactory
-        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
-        RegistrationPage registrationPage = PageFactory.initElements(driver, RegistrationPage.class);
-        DashboardPage dashboardPage = PageFactory.initElements(driver, DashboardPage.class);
-        WizardStep1Page wizardStep1Page = PageFactory.initElements(driver, WizardStep1Page.class);
+        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
 
-
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
 
 
 }
+
+
--- src/test/java/Tests/InventoryTests.java
+++ src/test/java/Tests/InventoryTests.java
@@ -1,8 +1,42 @@
 package Tests;
 
+import org.testng.annotations.Test;
+
+
+
 public class InventoryTests extends Base{
 
+    public void verifyHomePageIsDisplayed() {
+        homePage.verifyHomePageIsDisplayed();
+    }
 
-
-
+    // top-level starter: no dependsOnMethods
+    @Test(dependsOnMethods = {" verifyHomePageIsDisplayed"})
+    public void clickLearningMaterial() {
+        homePage.clickLearningMaterial();
+    }
 }
+
+//    // depends on the starter
+//    @Test(dependsOnMethods = {"setupInventory"})
+//    public void addItemToInventory() {
+//        // test adding an item
+//    }
+//
+//    // depends on addItemToInventory (will run after it)
+//
+//    @Test(dependsOnMethods = {"addItemToInventory"})
+//    public void updateItemQuantity() {
+//        // test updating quantity
+//    }
+//
+//
+//    @AfterTest
+//    public void closeBrowser() {
+//        driver.quit();
+//    }
+//}
+//
+
+
+
